147f6224eabd74423935826cf679c04c6aa99732
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2025-09-27 18:40:53 +0500

    wants appointment

--- frontline/helpers.py
+++ frontline/helpers.py
@@ -1,1 +1,11 @@
 ####
+
+from .models import WantAppointment
+from django.core.exceptions import ObjectDoesNotExist
+
+def wants_appointment(session_id: str) -> bool | None:
+    try:
+        record = WantAppointment.objects.get(session_id=session_id)
+        return record.wants_appointment
+    except ObjectDoesNotExist:
+        return None
